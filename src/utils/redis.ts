edeefb051fa3c5bb1c3cbb5c35546a73c8b44b48
coinlordd <35095979+coinlordd@users.noreply.github.com>
2022-01-30 00:03:24 +0100

    Update redis.ts

--- src/utils/redis.ts
+++ src/utils/redis.ts
@@ -1,2 +1,25 @@
-const redis = require('async-redis')
-export const redisClient = redis.createClient()
+const redis = require('redis')
+const asyncRedis = require('async-redis')
+
+let config =
+  process.env.NODE_ENV === 'production' && process.env.REDIS_URL
+    ? {
+        url: process.env.REDIS_URL,
+        socket: {
+          tls: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : null
+
+const client = redis.createClient(config)
+
+client.on('error', (error: Error) => {
+  console.error('Redis error encountered: ', error)
+})
+
+client.on('connect', () => {
+  console.log('Redis connection established')
+})
+
+export const redisClient = asyncRedis.decorate(client)
