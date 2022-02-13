9d927b7491f81bf38b7a73a432e48e4785c87ea0
coinlordd <35095979+coinlordd@users.noreply.github.com>
2022-02-13 15:34:48 +0100

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,21 +1,11 @@
 export const CORS_WHITELIST = [
   'http://localhost:3000',
 
-  // Raw domain
   'https://dsynths-app-v2.vercel.app',
   'http://dsynths-app-v2.vercel.app',
 
-  // Client production with www
-  'https://www.dsynths.com',
-  'http://www.dsynths.com',
-
-  // Client production without www
-  'https://dsynths.com',
-  'http://dsynths.com',
-
-  // Client staging
-  'https://staging.dsynths.com',
-  'http://staging.dsynths.com',
+  'https://app.dsynths.com',
+  'http://app.dsynths.com',
 ]
 
 export const CACHE_EXPIRE_STOCKS_SEC = 10 * 60
